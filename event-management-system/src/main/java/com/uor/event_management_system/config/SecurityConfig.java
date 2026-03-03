e84b05b939c03e19ee49070f11e76719a124c095
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2026-03-03 21:21:09 +0530

    understanding concepts

--- event-management-system/src/main/java/com/uor/event_management_system/config/SecurityConfig.java
+++ event-management-system/src/main/java/com/uor/event_management_system/config/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.uor.event_management_system.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+    }
 }
