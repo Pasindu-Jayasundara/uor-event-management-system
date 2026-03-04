4be7d7211f4185aa94e1b5f064628fa5ebd84b3c
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2026-03-05 00:23:50 +0530

    paths login logout sucurity configurations

---
+++ event-management-system/src/main/java/com/uor/event_management_system/config/FormLoginSecurityConfig.java
@@ -0,0 +1,76 @@
+package com.uor.event_management_system.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@Configuration
+public class FormLoginSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+            @Override
+            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+
+                authorizationManagerRequestMatcherRegistry
+                        .requestMatchers("/admin/**","/user/**").authenticated()
+                        .anyRequest().permitAll();
+            }
+        }).formLogin(new Customizer<FormLoginConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(FormLoginConfigurer<HttpSecurity> httpSecurityFormLoginConfigurer) {
+
+                httpSecurityFormLoginConfigurer
+                        .loginPage("/login")
+                        .successHandler(new AuthenticationSuccessHandler() {
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+                                String role = authentication.getAuthorities().iterator().next().getAuthority();
+
+                                if (role.equals("ROLE_ADMIN")) {
+                                    response.sendRedirect("/admin/dashboard");
+                                } else if (role.equals("ROLE_USER")) {
+                                    response.sendRedirect("/user/dashboard");
+                                } else {
+                                    response.sendRedirect("/");
+                                }
+                            }
+                        })
+                        .failureHandler(new AuthenticationFailureHandler() {
+                            @Override
+                            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                                response.sendRedirect("/login?error=true&msg="+exception.getMessage());
+                            }
+                        })
+                        .permitAll();
+            }
+        }).logout(new Customizer<LogoutConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(LogoutConfigurer<HttpSecurity> httpSecurityLogoutConfigurer) {
+
+                httpSecurityLogoutConfigurer
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/");
+            }
+        });
+
+        return http.build();
+    }
+}
