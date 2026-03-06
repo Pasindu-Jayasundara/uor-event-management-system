8750b6ffc178bb89a6ba6851ef78c9ca3b06256f
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2026-03-06 07:51:03 +0530

    paths login logout sucurity configurations

--- event-management-system/src/main/java/com/uor/event_management_system/controller/PagePathMapping.java
+++ event-management-system/src/main/java/com/uor/event_management_system/controller/PagePathMapping.java
@@ -1,20 +1,24 @@
 package com.uor.event_management_system.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PagePathMapping {
 
     @GetMapping("/login")
-    public String login(Model model){
+    public String login(){
         return "login";
     }
 
-    @GetMapping("/logout")
-    public String logout(){
-        return "logout";
+    @GetMapping("/admin/dashboard")
+    public String adminDashboard(){
+        return "admin/dashboard";
+    }
+
+    @GetMapping("/user/dashboard")
+    public String userDashboard(){
+        return "user/dashboard";
     }
 
 }
