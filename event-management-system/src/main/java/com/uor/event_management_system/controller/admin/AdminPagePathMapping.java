4fbc8e2716f505bb8af1df7224d36a26a290d176
danuja-illeperuma <ictdanu@gmail.com>
2026-03-08 22:00:19 +0530

    error fixed login db and register

--- event-management-system/src/main/java/com/uor/event_management_system/controller/admin/AdminPagePathMapping.java
+++ event-management-system/src/main/java/com/uor/event_management_system/controller/admin/AdminPagePathMapping.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PagePathMapping {
+public class AdminPagePathMapping {
 
     @GetMapping("/admin/dashboard")
     public String adminDashboard(){
--- event-management-system/src/main/java/com/uor/event_management_system/controller/common/CommonPagePathMapping.java
+++ event-management-system/src/main/java/com/uor/event_management_system/controller/common/CommonPagePathMapping.java
@@ -2,10 +2,8 @@ package com.uor.event_management_system.controller.common;
 
 import com.uor.event_management_system.dto.LoginDto;
 import com.uor.event_management_system.dto.RegisterDto;
-import com.uor.event_management_system.service.LoginUserService;
 import com.uor.event_management_system.service.RegisterUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class PagePathMapping {
+public class CommonPagePathMapping {
 
     @Autowired
     private RegisterUserService registerUserService;
