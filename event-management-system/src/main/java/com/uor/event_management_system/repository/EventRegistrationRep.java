28f3e125c963abc5a6c52177cfb84396f774eb69
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2026-04-20 00:33:35 +0530

    Revert "Pasi"

---
+++ event-management-system/src/main/java/com/uor/event_management_system/repository/EventRegistrationRep.java
@@ -0,0 +1,15 @@
+package com.uor.event_management_system.repository;
+
+import com.uor.event_management_system.enums.EventRegistrationStatus;
+import com.uor.event_management_system.model.EventEntity;
+import com.uor.event_management_system.model.EventRegistration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRegistrationRep extends JpaRepository<EventRegistration, Integer> {
+
+
+    int countByUser_IdAndStatus(Integer userId, EventRegistrationStatus status);
+    int countByEvent_IdAndStatus(Integer eventId, EventRegistrationStatus status);
+
+
+}
---
+++ event-management-system/src/main/java/com/uor/event_management_system/repository/EventRepository.java
@@ -0,0 +1,22 @@
+package com.uor.event_management_system.repository;
+
+import com.uor.event_management_system.enums.EventStatus;
+import com.uor.event_management_system.model.EventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<EventEntity, Integer> {
+    List<EventEntity> findByTitleContainingAndStatus(String keyword, EventStatus status);
+
+    List <EventEntity> findByEventCategoryAndStatus(String eventCategory, EventStatus status);
+
+    List<EventEntity> findByEventDateTimeAfterAndStatus(LocalDateTime dateTime, EventStatus status);
+
+    List<EventEntity> findByEventDateTimeBeforeAndStatus(LocalDateTime dateTime, EventStatus status);
+    List<EventEntity> findByStatus(EventStatus status);
+    int countByStatus(EventStatus status);
+
+}
