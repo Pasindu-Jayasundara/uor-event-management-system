107f6a631eb9cee5b4240ea78bfb441836bc78d2
danuja-illeperuma <ictdanu@gmail.com>
2026-04-26 12:49:31 +0800

    updated all

--- event-management-system/src/main/java/com/uor/event_management_system/controller/home/HomePagePathMapping.java
+++ event-management-system/src/main/java/com/uor/event_management_system/controller/home/HomePagePathMapping.java
@@ -86,6 +86,7 @@ public class HomePagePathMapping {
         response.setHeader("Pragma", "no-cache");
         response.setDateHeader("Expires", 0);
 
+        model.addAttribute("page", "event");
         return "homepage";
 
     }
